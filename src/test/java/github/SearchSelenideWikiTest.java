37553ad9c73d6b869365642ca586a62d29e51b5d
sergejgluhov <arane1223@gmail.com>
2025-08-13 08:07:57 +0500

    deleted import

--- src/test/java/github/SearchSelenideWikiTest.java
+++ src/test/java/github/SearchSelenideWikiTest.java
@@ -1,10 +1,7 @@
 package github;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
